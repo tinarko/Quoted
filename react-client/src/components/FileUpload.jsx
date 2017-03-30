b8c0f7662021ffb5a6d62aa2eaf8033e85c8b98b
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-29 19:05:44 -0700

    change fileUpload to use react-dropzone-component

--- react-client/src/components/FileUpload.jsx
+++ react-client/src/components/FileUpload.jsx
@@ -1,63 +1,52 @@
 import React from 'react';
-import Dropzone from 'react-dropzone';
-import $ from 'jquery';
+import ReactDOM from 'react-dom';
+import DropzoneComponent from 'react-dropzone-component';
 
 class FileUpload extends React.Component {
+  constructor(props) {
+    super(props);
 
-  onDrop (acceptedFiles, rejectedFiles) {
+    // For a full list of possible configurations,
+    // please consult http://www.dropzonejs.com/#configuration
+    this.djsConfig = {
+      addRemoveLinks: true,
+      acceptedFiles: 'image/jpeg,image/png,image/gif'
+    };
 
-    // $http({
-    //   method: 'POST',
-    //   url: '/onEnd', 
-    //   data: formData,
-    //   responseType: 'arraybuffer',
-    //   contentType: false,
-    //   transformRequest: angular.identity,
-    //   processData: false,
-    //   headers: {'Content-type': undefined}
-    // });
+    this.componentConfig = {
+      iconFiletypes: ['.jpg', '.png', '.gif'],
+      showFiletypeIcon: true,
+      postUrl: '/user/addcontacts'
+    };
 
-    console.log('Accepted files: ', acceptedFiles);
-    console.log('Rejected files: ', rejectedFiles);
-    let formData = new FormData();
-    formData.append('recording', acceptedFiles[0]);
-    $.ajax({
-      // url: '/user/addcontacts',
-      // type: 'POST',
-      // data: formData,
-      // cache: false,
-      // dataType: 'json',
-      // processData: false,
-      // contentType: false,
-      method: 'POST',
-      url: '/user/addcontacts', 
-      data: formData,
-      // responseType: 'arraybuffer',
-      dataType: 'json',
-      contentType: false,
-      // transformRequest: angular.identity,
-      processData: false,
-      headers: {'Content-type': undefined},
-      success: function(data) {
-        console.log('this is onDrop ajax success', data);
-      },
-      error: function(jqXHR, textStatus, errorThrown) { 
-        console.log(JSON.stringify(jqXHR)); 
-        console.log('AJAX error: ' + textStatus + ' : ' + errorThrown); 
-      }
-    });
+    // If you want to attach multiple callbacks, simply
+    // create an array filled with all your callbacks.
+    this.callbackArray = [() => console.log('Hi!'), () => console.log('Ho!')];
+
+    // Simple callbacks work too, of course
+    this.callback = () => console.log('Hello!');
+
+    this.success = file => console.log('uploaded', file);
+
+    this.removedfile = file => console.log('removing...', file);
+
+    this.dropzone = null;
   }
 
-  render () {
-    return (
-        <div>
-          <Dropzone onDrop={this.onDrop}>
-            <div>Try dropping some files here, or click to select files to upload.</div>
-          </Dropzone>
-        </div>
-    );
-  }
+  render() {
+    const config = this.componentConfig;
+    const djsConfig = this.djsConfig;
 
+    // For a list of all possible events (there are many), see README.md!
+    const eventHandlers = {
+      init: dz => this.dropzone = dz,
+      drop: this.callbackArray,
+      addedfile: this.callback,
+      success: this.success,
+      removedfile: this.removedfile
+    };
+    return <DropzoneComponent config={config} eventHandlers={eventHandlers} djsConfig={djsConfig} />;
+  }
 }
 
 export default FileUpload;
