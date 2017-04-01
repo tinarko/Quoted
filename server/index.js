7d1c4fc85e43a5a3c1ab4de058776e9709c685f0
Tina Ko <tinarchan@gmail.com>
2017-04-01 11:52:45 -0700

    (feat/threads) start to add db changes for threads

--- server/index.js
+++ server/index.js
@@ -65,6 +65,7 @@ app.post('/messages', handler.textBusinesses);
 app.post('/', handler.receiveText); // SHOULD CHANGE ngrok path to '/sms' if there is time.
 // app.post('/sms', handler.receiveText); 
 app.get('/findText/:number', handler.findResponsesFromContactNumber);
+// app.post('/createNewThread', handler.createNewThread);
 
 app.post('/call', handler.callBusinesses)  
 app.post('/voice', handler.setVoiceMessage);
